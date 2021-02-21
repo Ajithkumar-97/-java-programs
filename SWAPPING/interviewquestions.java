a3719534e0ea7df8de214f37791469a1af31379a
Ajith kumar <76963226+Ajithkumar-97@users.noreply.github.com>
2021-02-21 12:06:04 +0530

    SWAPPING ADDED
    swapping of the string

---
+++ SWAPPING/interviewquestions.java
@@ -0,0 +1,18 @@
+package java;
+
+public class interviewquestions {
+
+	
+		public static void main(String[] args) {
+				
+				String mysalary="fifty";
+				String ssalary="fourty";
+				System.out.println ("befor swapping"+"mine:"+mysalary+"superior:"+ssalary);
+				
+				String temp = mysalary;
+				mysalary=ssalary;
+				ssalary=temp;
+				
+				System.out.println("after swapping"+"mine:"+mysalary+"superior:"+ssalary);
+		}
+	}
