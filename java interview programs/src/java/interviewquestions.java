d6f44d1f235af1c5a6291b9e3ea1500055d37507
Ajith kumar <76963226+Ajithkumar-97@users.noreply.github.com>
2021-02-21 11:15:07 +0530

    Add files via upload

---
+++ java interview programs/src/java/interviewquestions.java
@@ -0,0 +1,18 @@
+package java;
+
+public class interviewquestions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String reverse="ajith";
+		
+		StringBuffer buffer=new StringBuffer();
+		
+		buffer.append(reverse);
+		System.out.println ("the reversable name is");
+		
+		
+	}
+
+}
